ba4d67d98ebc85c5d0182cf44cf7a1e19727f253
LebedKevin <lebed.kevin@gmail.com>
2021-03-28 20:38:43 -0700

    act 9

--- Unit 09/src/DeckTester.java
+++ Unit 09/src/DeckTester.java
@@ -5,18 +5,19 @@ public class DeckTester {
 
 	/**
 	 * The main method in this class checks the Deck operations for consistency.
-	 *	@param args is not used.
+	 * 
+	 * @param args is not used.
 	 */
 	public static void main(String[] args) {
-		String[] ranks1 = {"A", "B", "C"};
-		String[] ranks2 = {"Jack", "Queen", "King"};
-		String[] ranks3 = {"10", "9", "8"};
-		String[] suits1 = {"Spades"};
-		String[] suits2 = {"Spades", "Clubs"};
-		String[] suits3 = {"Hearts", "Clubs", "Diamonds"};
-		int[] vaules1 = {1, 2, 3};
-		int[] vaules2 = {4, 5, 6};
-		int[] vaules3 = {10, 9, 8};
+		String[] ranks1 = { "A", "B", "C" };
+		String[] ranks2 = { "Jack", "Queen", "King" };
+		String[] ranks3 = { "10", "9", "8" };
+		String[] suits1 = { "Spades" };
+		String[] suits2 = { "Spades", "Clubs" };
+		String[] suits3 = { "Hearts", "Clubs", "Diamonds" };
+		int[] vaules1 = { 1, 2, 3 };
+		int[] vaules2 = { 4, 5, 6 };
+		int[] vaules3 = { 10, 9, 8 };
 		Deck deck1 = new Deck(ranks1, suits1, vaules1);
 		Deck deck2 = new Deck(ranks2, suits2, vaules2);
 		Deck deck3 = new Deck(ranks3, suits3, vaules3);
